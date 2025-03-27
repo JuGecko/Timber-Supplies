0f4dfde7c1bc4f007d23e5f8fe6b39b6df1ef6b7
Julia Grzeskowiak <grzesjul@student.cvut.cz>
2025-03-27 14:27:13 +0100

    Added mobile menu modal. Finished navbar, yet some elements are not showing correctly

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "./scenes/navbar";
 import { SelectedPage } from "./shared/types";
 
 function App() {
-const [selectedPage, setselectedPage] = useState<SelectedPage>(
+const [selectedPage, setSelectedPage] = useState<SelectedPage>(
   SelectedPage.Home
 );
+const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+useEffect(() => {
+  const handleScroll = () => {
+    if (window.scrollY === 0) {
+      setIsTopOfPage(true);
+      setSelectedPage(SelectedPage.Home);
+    }
+    if (window.scrollY !== 0) setIsTopOfPage(false);
+  }
+  window.addEventListener("scroll", handleScroll); 
+  //if you dont add this line you might have memory leaks
+  return () => window.removeEventListener("scroll", handleScroll);
+}, []);
 
   return <div className="app bg-gray-20">
     <Navbar 
+      isTopOfPage={isTopOfPage}
       selectedPage={selectedPage}
-      setselectedPage={setselectedPage}
+      setselectedPage={setSelectedPage}
     />
   </div>;
 }
--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -7,18 +7,20 @@ import useMediaQuery from "../../hooks/useMediaQuery";
 import ActionButton from "../../shared/ActionButton";
 
 type Props = {
+    isTopOfPage: boolean;
     selectedPage: SelectedPage;
     setselectedPage: (value: SelectedPage) => void;
 };
 
-const Navbar = ({ selectedPage, setselectedPage }: Props) => {
+const Navbar = ({ isTopOfPage, selectedPage, setselectedPage }: Props) => {
     const flexBetween = "flex items-center justify-between"
     const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
-    const isAboveMediumScreens = useMediaQuery("min-width: 1060px");
-    
+    const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+    const navbarBackground = isTopOfPage ? "" : "bg-primary-100 drop-shadow"; 
+
 return (
     <nav>
-    <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+    <div className={`${navbarBackground} ${flexBetween} fixed top-0 z-30 w-full py-6`}>
         <div className={`${flexBetween} mx-auto w-5/6`}>
             <div className={`${flexBetween} w-full gap`}>
                 {/* LEFT SIDE */}
@@ -26,24 +28,7 @@ return (
                 {/* RIGHT SIDE */} 
                 {isAboveMediumScreens ? ( 
                 <div className={`${flexBetween} w-full`}>
-                <div className={`${flexBetween} gap-8 text-sm`}>
-                    <Link page="Home" 
-                    selectedPage={selectedPage}
-                    setselectedPage={setselectedPage} 
-                    />
-                    <Link page="Our Story"
-                    selectedPage={selectedPage}
-                    setselectedPage={setselectedPage} 
-                    />
-                    <Link page="Pricing"
-                    selectedPage={selectedPage}
-                    setselectedPage={setselectedPage} 
-                    />
-                    <Link page="Contact us"
-                    selectedPage={selectedPage}
-                    setselectedPage={setselectedPage} 
-                    />
-                </div>
+                
                 <div className={`${flexBetween} gap-8`}>
                     <p>Sign In</p>
                     <ActionButton setSelectedPage={setselectedPage}>
@@ -62,8 +47,45 @@ return (
         </div>
     </div>
     </div>
+
+    {/* MOBILE MENU MODAL */}
+    {!isAboveMediumScreens && isMenuToggled && (
+    <div className="fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl">
+
+    {/* CLOSE ICON */}
+        <div className="flex justify-end p-12">
+            <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+                <XMarkIcon className="h-6 w-6 text-gray-400" />
+            </button>
+        </div>
+
+    {/* MENU ITEMS */}
+        <div className="ml-[33%] flex flex-col gap-10 text-2xl">
+            <Link 
+            page="Home" 
+            selectedPage={selectedPage}
+            setselectedPage={setselectedPage} 
+            />
+            <Link
+            page="Our Story"
+            selectedPage={selectedPage}
+            setselectedPage={setselectedPage} 
+            />
+            <Link
+            page="Pricing"
+            selectedPage={selectedPage}
+            setselectedPage={setselectedPage} 
+            />
+            <Link
+            page="Contact us"
+            selectedPage={selectedPage}
+            setselectedPage={setselectedPage} 
+            />
+        </div>
+
+    </div>
+    )}
   </nav>
-)
-};
+)};
 
 export default Navbar;
