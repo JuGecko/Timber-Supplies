c513a96667fdc0b24e642cc4e0a925b45c046762
Julia Grzeskowiak <grzesjul@student.cvut.cz>
2025-03-30 09:02:55 +0200

    Animations for the home page

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -7,6 +7,7 @@ import SponsorRedbull from '@/assets/SponsorRedbull.png'
 import SponsorForbes from '@/assets/SponsorForbes.png'
 import SponsorFortune from '@/assets/SponsorFortune.png'
 import AnchorLink from 'react-anchor-link-smooth-scroll';
+import { motion } from 'framer-motion';
 
 type Props = {
     setSelectedPage: (value: SelectedPage) => void;
@@ -18,12 +19,25 @@ const Home = (props: Props) => {
     /*We are applying h-full only if the screen is above md size. py means padding on top and bottom (y-axis).*/
     return <section id="home" className='gap-16 bg-gray-20 py-10 md:h-full md:pb-0'>
     {/* IMAGE AND MAIN HEADER */}
-    <div className='md:flex mx-auto w-5/6 items-center justify-center md:h-5/6'>
+    <motion.div className='md:flex mx-auto w-5/6 items-center justify-center md:h-5/6'
+    // switch on and off the navbar red color while scrolling
+    onViewportEnter={() => setSelectedPage(SelectedPage.Home)}
+    >
         {/* MAIN HEADER */}
         <div className='z-10 mt-32 md:basis-3/5'>
             {/* HEADINGS */}
             {/* the section below will be pushed a lil higher */}
-            <div className='md:-mt-20'>
+            <motion.div
+             className='md:-mt-20'
+             initial="hidden"
+             whileInView="visible"
+             viewport={{ once: true, amount: 0.5 }}
+             transition={{ duration: 0.5 }}
+             variants={{
+                hidden: { opacity: 0, x: -50 },
+                visible: { opacity: 1, x: 0 },
+             }}
+             >
                 <div className='relative'>
                     <div className='before:absolute before:-top-20 before:-left-20 before:z-[-1] md:before:content-evolvetext'>
                         <img src={HomePageText} alt="home-page-text" />
@@ -33,10 +47,20 @@ const Home = (props: Props) => {
                     Lorem ipsum dolor sit amet consectetur, adipisicing elit.
                     Natus totam, sint asperiores alias quisquam eius. alias quisquam eius.
                 </p>
-            </div>
+            </motion.div>
 
         {/* ACTIONS */}
-        <div className='mt-8 flex items-center gap-8'>
+        <motion.div
+        className='mt-8 flex items-center gap-8'
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        transition={{ delay: 0.2, duration: 0.5 }}
+        variants={{
+           hidden: { opacity: 0, x: -50 },
+           visible: { opacity: 1, x: 0 },
+        }}
+        >
             <ActionButton setSelectedPage={setSelectedPage}>
             Order Now
             </ActionButton>
@@ -47,26 +71,25 @@ const Home = (props: Props) => {
             >
             <p>Learn More</p>
             </AnchorLink>
-
-        </div>
+        </motion.div>
         </div>
         {/* IMAGE */}
         <div className='flex basis-3/5 justify-center md:z-10 md:ml-40 md:mt-16 md:justify-items-end'>
             <img src={HomePageGraphic} alt="home-pageGraphic" />
         </div>
-    </div>
-    {/* SPONSORS */}
-    {isAboveMediumScreens && (
-    <div className='h-[150px] w-full bg-primary-100 py-10'>
-        <div className='mx-auto w-5/6'>
-            <div className='flex w-3/5 itmes-center justify-between gap-8'>
-                <img src={SponsorRedbull} alt="redbull-sposor" />
-                <img src={SponsorForbes} alt="forbes-sposor" />
-                <img src={SponsorFortune} alt="fortune-sposor" />
+    </motion.div>
+        {/* SPONSORS */}
+        {isAboveMediumScreens && (
+        <div className='h-[150px] w-full bg-primary-100 py-10'>
+            <div className='mx-auto w-5/6'>
+                <div className='flex w-3/5 itmes-center justify-between gap-8'>
+                    <img src={SponsorRedbull} alt="redbull-sposor" />
+                    <img src={SponsorForbes} alt="forbes-sposor" />
+                    <img src={SponsorFortune} alt="fortune-sposor" />
+                </div>
             </div>
         </div>
-    </div>
-    )}
+        )}
     </section>
 }
 
