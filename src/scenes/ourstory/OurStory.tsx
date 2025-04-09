1e8e105c182961241570d3f9aff073b38b5c6669
Julia Grzeskowiak <grzesjul@student.cvut.cz>
2025-04-09 20:56:42 +0200

    OurStory page is done. The icon issue remains.

--- src/scenes/ourstory/OurStory.tsx
+++ src/scenes/ourstory/OurStory.tsx
@@ -1,7 +1,13 @@
-import { JSX } from 'react'
+
 import { SelectedPage } from '../../shared/types';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
+import { motion } from 'framer-motion';
+import { JSX } from 'react';
 
+const childVariant = {
+  hidden: { opacity: 0, scale: 0.9 },
+  visible: { opacity: 1, scale: 1},
+}
 type Props = {
     icon: JSX.Element;
     title: string;
@@ -11,7 +17,9 @@ type Props = {
 
 const OurStory = ({icon, title, description, setSelectedPage}: Props) => {
   return (
-    <div className='mt-5 rounded-md border-2 border-gray-100 px-5 py-16 text-center'>
+    <motion.div 
+    variants={childVariant}
+    className='mt-5 rounded-md border-2 border-gray-100 px-5 py-16 text-center'>
         <div className='mb-4 flex justify-center'>
             <div className='rounded-full border-2 border-gray-100 bg-primary-100 p-4'>
             {icon}
@@ -27,6 +35,8 @@ const OurStory = ({icon, title, description, setSelectedPage}: Props) => {
         >
         <p>Learn More</p>
         </AnchorLink>
-    </div>
+    </motion.div>
   )
 }
+
+export default OurStory;
